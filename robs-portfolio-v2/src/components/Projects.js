f76b70416df2b11078829e02d0df435ce587afd0
Rob <rclaus29@gmail.com>
2023-08-09 17:35:55 -0400

    updated tabs in projects

--- robs-portfolio-v2/src/components/Projects.js
+++ robs-portfolio-v2/src/components/Projects.js
@@ -43,13 +43,13 @@ export const Projects = () => {
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                             <Nav.Item>
-                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                <Nav.Link eventKey="first">Current Projects</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                <Nav.Link eventKey="second">Future Projects</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                <Nav.Link eventKey="third">Future Projects</Nav.Link>
                             </Nav.Item>
                         </Nav>
                         <Tab.Content>
